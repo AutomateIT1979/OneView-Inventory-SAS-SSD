d23fc966b380b414a342b32fc80969bb07b948da
GNOME IT <barioucha@gmail.com>
2024-05-30 21:43:27 +0200

    Update OV-Get-Disks.ps1

--- OV-Get-Disks.ps1
+++ OV-Get-Disks.ps1
@@ -19,47 +19,58 @@ foreach ($appliance in $appliances) {
     try {
         # Connect to the OneView appliance
         Connect-OVMgmt -Hostname $fqdn -Credential $credential
+
         # Get the server objects for Gen10 servers
         $servers = Get-OVServer | Where-Object { $_.model -match 'Gen10' }
+
         foreach ($server in $servers) {
             # Construct the URI for local storage details
             $localStorageUri = $server.uri + '/localStorage'
+
             # Retrieve the local storage details (using Send-OVRequest)
             $localStorageDetails = Send-OVRequest -Uri $localStorageUri -Method GET
+
             # Check if localStorageDetails is not null
             if ($null -ne $localStorageDetails) {
-                # Split the Servername values
-                $serverNames = $server.name -split ','
+                # Ensure serverName is always an array, even if it's just one value
+                $serverNames = @($server.name)
+                if ($serverNames -is [string]) {
+                    $serverNames = $serverNames -split ','
+                }
+
                 # Iterate over each physical drive to create individual records
                 foreach ($drive in $localStorageDetails.PhysicalDrives) {
-                    $info = [PSCustomObject]@{
-                        ApplianceFQDN             = $fqdn
-                        Servername                = $serverNames # Use the split Servername values
-                        AdapterType               = $localStorageDetails.AdapterType
-                        BackupPowerSourceStatus   = $localStorageDetails.BackupPowerSourceStatus
-                        CacheMemorySizeMiB        = $localStorageDetails.CacheMemorySizeMiB
-                        CurrentOperatingMode      = $localStorageDetails.CurrentOperatingMode
-                        ExternalPortCount         = $localStorageDetails.ExternalPortCount
-                        FirmwareVersion           = $localStorageDetails.FirmwareVersion.Current
-                        InternalPortCount         = $localStorageDetails.InternalPortCount
-                        Location                  = $localStorageDetails.Location
-                        LocationFormat            = $localStorageDetails.LocationFormat
-                        Model                     = $localStorageDetails.Model
-                        Name                      = $localStorageDetails.Name
-                        SerialNumber               = $localStorageDetails.SerialNumber
-                        Status                    = $localStorageDetails.Status
-                        Drive_BlockSizeBytes       = $drive.BlockSizeBytes
-                        Drive_CapacityLogicalBlocks = $drive.CapacityLogicalBlocks
-                        Drive_CapacityMiB          = $drive.CapacityMiB
-                        Drive_EncryptedDrive       = $drive.EncryptedDrive
-                        Drive_FirmwareVersion      = $drive.FirmwareVersion
-                        Drive_Location            = $drive.Location
-                        Drive_Model               = $drive.Model
-                        Drive_SerialNumber        = $drive.SerialNumber
-                        Drive_Status              = $drive.Status
-                    }
-                    # Add the collected information to the data array
-                    $data += $info
+                    foreach ($serverName in $serverNames) {
+                        # Loop for each Servername value
+                        $info = [PSCustomObject]@{
+                            ApplianceFQDN               = $fqdn
+                            Servername                  = $serverName.Trim()  # Remove extra spaces
+                            AdapterType                 = $localStorageDetails.AdapterType
+                            BackupPowerSourceStatus     = $localStorageDetails.BackupPowerSourceStatus
+                            CacheMemorySizeMiB          = $localStorageDetails.CacheMemorySizeMiB
+                            CurrentOperatingMode        = $localStorageDetails.CurrentOperatingMode
+                            ExternalPortCount           = $localStorageDetails.ExternalPortCount
+                            FirmwareVersion             = $localStorageDetails.FirmwareVersion.Current
+                            InternalPortCount           = $localStorageDetails.InternalPortCount
+                            Location                    = $localStorageDetails.Location
+                            LocationFormat              = $localStorageDetails.LocationFormat
+                            Model                       = $localStorageDetails.Model
+                            Name                        = $localStorageDetails.Name
+                            SerialNumber                = $localStorageDetails.SerialNumber
+                            Status                      = $localStorageDetails.Status
+                            Drive_BlockSizeBytes        = $drive.BlockSizeBytes
+                            Drive_CapacityLogicalBlocks = $drive.CapacityLogicalBlocks
+                            Drive_CapacityMiB           = $drive.CapacityMiB
+                            Drive_EncryptedDrive        = $drive.EncryptedDrive
+                            Drive_FirmwareVersion       = $drive.FirmwareVersion
+                            Drive_Location              = $drive.Location
+                            Drive_Model                 = $drive.Model
+                            Drive_SerialNumber          = $drive.SerialNumber
+                            Drive_Status                = $drive.Status
+                        }
+                        # Add the collected information to the data array
+                        $data += $info
+                    }  
                 }
             }
         }
