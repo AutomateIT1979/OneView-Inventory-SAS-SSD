e84c8d8d0e8d4154d433d05f88ce2c0d37cb27df
GNOME IT <barioucha@gmail.com>
2024-06-01 16:34:49 +0200

    Update OV-Get-Disks.ps1

--- OV-Get-Disks.ps1
+++ OV-Get-Disks.ps1
@@ -40,28 +40,34 @@ foreach ($appliance in $appliances) {
             if ($null -ne $localStorageDetails) {
                 foreach ($drive in $localStorageDetails.Data.PhysicalDrives) {
                     $info = [PSCustomObject]@{
-                        ApplianceFQDN             = $fqdn
-                        Servername                = $server.name.Trim()  # Remove extra spaces
-                        AdapterType               = $localStorageDetails.Data.AdapterType
-                        CurrentOperatingMode      = $localStorageDetails.Data.CurrentOperatingMode
-                        ExternalPortCount         = $localStorageDetails.Data.ExternalPortCount
-                        FirmwareVersion           = $localStorageDetails.Data.FirmwareVersion.Current.VersionString
-                        InternalPortCount         = $localStorageDetails.Data.InternalPortCount
-                        Location                  = $localStorageDetails.Data.Location
-                        LocationFormat            = $localStorageDetails.Data.LocationFormat
-                        Model                     = $localStorageDetails.Data.Model
-                        Name                      = $localStorageDetails.Data.Name
-                        SerialNumber              = $localStorageDetails.Data.SerialNumber
-                        Status                    = $localStorageDetails.Data.Status
-                        Drive_BlockSizeBytes       = $drive.BlockSizeBytes
-                        Drive_CapacityLogicalBlocks = $drive.CapacityLogicalBlocks
-                        Drive_CapacityMiB          = $drive.CapacityMiB
-                        Drive_EncryptedDrive       = $drive.EncryptedDrive
-                        Drive_FirmwareVersion      = $drive.FirmwareVersion.Current.VersionString
-                        Drive_Location            = $drive.Location
-                        Drive_Model               = $drive.Model
-                        Drive_SerialNumber        = $drive.SerialNumber
-                        Drive_Status              = $drive.Status.Health
+                                                ApplianceFQDN              = $fqdn
+                        ServerName                 = $server.Name
+                        ServerStatus               = $server.Status
+                        ServerPower                = $server.PowerState
+                        ServerSerialNumber         = $server.SerialNumber
+                        ServerModel                = $server.Model
+                        AdapterType                = $localStorageDetails.Data.AdapterType
+                        CurrentOperatingMode       = $localStorageDetails.Data.CurrentOperatingMode                 
+                        FirmwareVersion            = $localStorageDetails.Data.FirmwareVersion.Current.VersionString
+                        InternalPortCount          = $localStorageDetails.Data.InternalPortCount
+                        Location                   = $localStorageDetails.Data.Location
+                        LocationFormat             = $localStorageDetails.Data.LocationFormat                      
+                        LogicalDriveNumbers        = ($localStorageDetails.Data.LogicalDrives | ForEach-Object { $_.LogicalDriveNumber }) -join ', '
+                        RaidValues                 = ($localStorageDetails.Data.LogicalDrives | ForEach-Object { $_.Raid }) -join ', '
+                        Model                      = $localStorageDetails.Data.Model
+                        DriveBlockSizeBytes        = $drive.BlockSizeBytes
+                        # Calculate the logical capacity in GB
+                        LogicalCapacityGB = [math]::Round(($drive.CapacityLogicalBlocks * $drive.BlockSizeBytes) / 1e9, 2)
+                        DriveEncryptedDrive        = $drive.EncryptedDrive
+                        DriveFirmwareVersion       = $drive.FirmwareVersion.Current.VersionString
+                        DriveInterfaceType         = $drive.InterfaceType
+                        DriveMediaType             = $drive.MediaType
+                        DriveLocation              = $drive.Location
+                        DriveModel                 = $drive.Model
+                        DriveSerialNumber          = $drive.SerialNumber
+                        DriveStatus                = $drive.Status.Health
+                        DriveState                 = $drive.Status.State
+                        "Drive Life Remaining (%)" = "{0}%" -f (100 - $drive.SSDEnduranceUtilizationPercentage)
                     }
 
                     # Add the collected information to the data array
