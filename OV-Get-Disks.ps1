41653dd6d421ee2ec7a1355e818289ea7a95f12f
GNOME IT <barioucha@gmail.com>
2024-06-01 16:07:17 +0200

    Update OV-Get-Disks.ps1

--- OV-Get-Disks.ps1
+++ OV-Get-Disks.ps1
@@ -8,91 +8,76 @@ Import-Module ImportExcel -Verbose
 # Define script paths and file names
 $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $csvPath = Join-Path $scriptPath "Appliances_liste.csv"
-$appliancesList = Import-Csv $csvPath
+$appliances = Import-Csv $csvPath
 
-# Define function for error handling and logging
-function Write-ErrorLog {
-    param([string]$errorMessage)
-    Write-Host $errorMessage -ForegroundColor Red
-    $errorMessage | Add-Content $logFile
-}
+# Initialize an array to hold the collected data
+$data = @()
 
-# Initialize data collection and error log file path
-$dataCollection = [System.Collections.Generic.List[object]]::new()
-$logFile = Join-Path $scriptPath "error_log.txt"
+# Log file for errors
+$logFile = Join-Path -Path $scriptPath -ChildPath "error_log.txt"
 
-# Check if log file exists, create it if not
-if (-not (Test-Path $logFile)) {
-    New-Item -Path $logFile -ItemType File -Force
-}
-
-# Connect to all appliances and gather information
-foreach ($appliance in $appliancesList) {
+# Loop through each appliance and retrieve the required information
+foreach ($appliance in $appliances) {
     $fqdn = $appliance.Appliance_FQDN
-
     try {
-        Write-Host "Connecting to OneView appliance: $fqdn" -ForegroundColor Green
-        $connectedSession = Connect-OVMgmt -Hostname $fqdn -Credential (Get-Credential -Message "Enter OneView credentials")
+        # Connect to the OneView appliance
+        Connect-OVMgmt -Hostname $fqdn -Credential $credential
 
-        $servers = Get-OVServer -ApplianceConnection $connectedSession | Where-Object { $_.model -match 'Gen10' }
+        # Get the server objects for Gen10 servers
+        $servers = Get-OVServer | Where-Object { $_.model -match 'Gen10' }
 
         foreach ($server in $servers) {
+            # Construct the URI for local storage details
             $localStorageUri = $server.uri + '/localStorage'
-            $localStorageDetails = Send-OVRequest -uri $localStorageUri
 
-            if ($localStorageDetails) {
-                foreach ($drive in $localStorageDetails.Data.PhysicalDrives) {
+            # Retrieve the local storage details (using Send-OVRequest)
+            $localStorageDetails = Send-OVRequest -Uri $localStorageUri -Method GET
+
+            # Check if localStorageDetails is not null
+            if ($null -ne $localStorageDetails) {
+                foreach ($drive in $localStorageDetails.PhysicalDrives) {
                     $info = [PSCustomObject]@{
-                        ApplianceFQDN              = $connectedSession.Name
-                        ServerName                 = $server.serverName.ToUpper()
-                        Name                       = ($server.Name -split ', ')[0]
-                        BayNumber                  = ($server.Name -split ', ')[-1]
-                        ServerStatus               = $server.Status
-                        ServerPower                = $server.PowerState
-                        ProcessorCoreCount         = $server.ProcessorCoreCount
-                        ProcessorCount             = $server.ProcessorCount
-                        ProcessorSpeedMhz          = $server.ProcessorSpeedMhz
-                        ProcessorType              = $server.ProcessorType
-                        ServerSerialNumber         = $server.SerialNumber
-                        ServerModel                = $server.Model
-                        AdapterType                = $localStorageDetails.Data.AdapterType
-                        CurrentOperatingMode       = $localStorageDetails.Data.CurrentOperatingMode
-                        FirmwareVersion            = $localStorageDetails.Data.FirmwareVersion.Current.VersionString
-                        InternalPortCount          = $localStorageDetails.Data.InternalPortCount
-                        Location                   = $localStorageDetails.Data.Location
-                        LocationFormat             = $localStorageDetails.Data.LocationFormat
-                        LogicalDriveNumbers        = ($localStorageDetails.Data.LogicalDrives.LogicalDriveNumber) -join ', '
-                        RaidValues                 = ($localStorageDetails.Data.LogicalDrives.Raid) -join ', '
-                        Model                      = $localStorageDetails.Data.Model
-                        DriveBlockSizeBytes        = $drive.BlockSizeBytes
-                        LogicalCapacityGB          = [math]::Round(($drive.CapacityLogicalBlocks * $drive.BlockSizeBytes) / 1e9, 2)
-                        DriveEncryptedDrive        = $drive.EncryptedDrive
-                        DriveFirmwareVersion       = $drive.FirmwareVersion.Current.VersionString
-                        DriveInterfaceType         = $drive.InterfaceType
-                        DriveMediaType             = $drive.MediaType
-                        DriveLocation              = $drive.Location
-                        DriveModel                 = $drive.Model
-                        DriveSerialNumber          = $drive.SerialNumber
-                        DriveStatus                = $drive.Status.Health
-                        DriveState                 = $drive.Status.State
-                        "Drive Life Remaining (%)" = "{0}%" -f (100 - $drive.SSDEnduranceUtilizationPercentage)
+                        ApplianceFQDN             = $fqdn
+                        Servername                = $server.name.Trim()  # Remove extra spaces
+                        AdapterType               = $localStorageDetails.AdapterType
+                        CurrentOperatingMode      = $localStorageDetails.CurrentOperatingMode
+                        ExternalPortCount         = $localStorageDetails.ExternalPortCount
+                        FirmwareVersion           = $localStorageDetails.FirmwareVersion.Current
+                        InternalPortCount         = $localStorageDetails.InternalPortCount
+                        Location                  = $localStorageDetails.Location
+                        LocationFormat            = $localStorageDetails.LocationFormat
+                        Model                     = $localStorageDetails.Model
+                        Name                      = $localStorageDetails.Name
+                        SerialNumber              = $localStorageDetails.SerialNumber
+                        Status                    = $localStorageDetails.Status
+                        Drive_BlockSizeBytes       = $drive.BlockSizeBytes
+                        Drive_CapacityLogicalBlocks = $drive.CapacityLogicalBlocks
+                        Drive_CapacityMiB          = $drive.CapacityMiB
+                        Drive_EncryptedDrive       = $drive.EncryptedDrive
+                        Drive_FirmwareVersion      = $drive.FirmwareVersion
+                        Drive_Location            = $drive.Location
+                        Drive_Model               = $drive.Model
+                        Drive_SerialNumber        = $drive.SerialNumber
+                        Drive_Status              = $drive.Status
                     }
-                    $dataCollection.Add($info)
+
+                    # Add the collected information to the data array
+                    $data += $info
                 }
             }
         }
+
+        Disconnect-OVMgmt -Hostname $fqdn
     }
     catch {
-        Write-ErrorLog "Error connecting to appliance ${fqdn}: $($_.Exception.Message)"
-    }
-    finally {
-        Write-Host "Disconnecting from OneView appliance: $fqdn" -ForegroundColor Green
-        Disconnect-OVMgmt -Hostname $fqdn
+        $errorMessage = "Error processing appliance ${fqdn}: $($_.Exception.Message)"
+        Write-Warning $errorMessage
+        $errorMessage | Add-Content -Path $logFile
     }
 }
 
 # Sorting and exporting data to CSV and Excel
-$sortedData = $dataCollection | Sort-Object -Property ApplianceFQDN, BayNumber -Descending
+$sortedData = $data | Sort-Object -Property ApplianceFQDN, Servername -Descending
 
 # Export data to CSV file (append mode)
 $sortedData | Export-Csv -Path "$scriptPath\LocalStorageDetails.csv" -NoTypeInformation -Append
